5e67466f34a00501fd5582078da3959cbc3f852b
Basem Osama <engbasemosama@gmail.com>
2025-01-08 22:45:58 +0200

    Add new extension `toFormattedTime` on `DateTime` to format time to localized time string

--- lib/src/extensions/date_extensions.dart
+++ lib/src/extensions/date_extensions.dart
@@ -1,10 +1,20 @@
 import 'package:intl/intl.dart';
+import 'package:playx_localization/src/playx_localization.dart';
 
 /// Extension functions to help operations on DateTime.
 extension DateExtensions on DateTime {
+  /// Converts the date to a formatted string.
   String toFormattedDate(
       {String format = 'dd-MM-yyyy hh:mm a', String? locale}) {
-    final df = DateFormat(format, locale);
+    final df = DateFormat(
+        format, locale ?? PlayxLocalization.currentLocale.languageCode);
+    return df.format(this);
+  }
+
+  /// Converts the date to a formatted time string.
+  String toFormattedTime({String format = 'hh:mm a', String? locale}) {
+    final df = DateFormat(
+        format, locale ?? PlayxLocalization.currentLocale.languageCode);
     return df.format(this);
   }
 }
